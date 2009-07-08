7aaa8688fcdc94a0d8da0a1dac967ec7a10654ad
alexgpg <alexgpg@gmail.com>
2009-07-09 00:47:04 +0400

    support config file added, but not used

---
+++ works/config.rb
@@ -0,0 +1,47 @@
+#!/usr/bin/ruby1.8
+require 'singleton'
+
+##
+# The Configuration handler.
+# Using for get configuration informations.
+#
+# Example 1:
+#	# Create config in programm
+#	Config.instance.setConfig do
+#		allow_domains	:all
+#		deny_domains	:no
+#		max_users 100
+#	end
+# Example 2:
+#	# Read config from file
+#	Config.instance.setConfigFromFile('test_wijet.conf')
+#
+# Format config file:
+#	field value
+#	other_fileld val
+#	...
+class Config
+	include Singleton
+
+	def setConfig &a_block
+		instance_eval &a_block
+	end
+	
+	def setConfigFromFile(a_fileName)
+		instance_eval( IO.read(a_fileName) )
+	end
+	
+	# Method for get and set fileld.
+	#
+	# Config.instance.aaa 10500 - set value for fileld "aaa".
+	#
+	# Config.instance.aaa - get fileld with name "aaa", it must be already initialized.
+	def method_missing a_name, *a_args, &a_block
+		if a_args.size==0
+			raise "Variable \"#{a_name}\" not exist"
+		end
+		instance_variable_set("@#{a_name}".to_sym, a_args[0])
+		self.class.send(:define_method, a_name, proc {instance_variable_get("@#{a_name}")})
+	end
+end
+
